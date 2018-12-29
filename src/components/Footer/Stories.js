4f1e74fedf6278181ba3c56e90f8fab18b2c2d38
cecil john tantay <cjbtantay@gmail.com>
2018-12-29 11:32:17 -0800

    removed logout button next to social

--- src/components/Footer/Stories.js
+++ src/components/Footer/Stories.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Stories = props => {
+const Stories = () => {
   return (
     <React.Fragment>
       <div className='footer-box stories-container'>
         <div className='box-left'>Stories</div>
         <div className='box-right'>Watch All</div>
       </div>
-      <div>
-        <form onSubmit={props.logout}>
-          <button>Log Out</button>
-        </form>
-      </div>
     </React.Fragment>
   );
 };
 
-Stories.propTypes = {
-  logout: PropTypes.func
-};
 export default Stories;
