efd5c9018330b9b4a69877e44866585eac71d822
cecil john tantay <cjbtantay@gmail.com>
2018-12-29 14:19:08 -0800

    fixing login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import '../style/App.scss';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  max-width: 980px;
+`;
+
+const TopContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid red;
+  width: 350px;
+  margin-top: 12px;
+  justify-content: center;
+`;
+
+const Form = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-flow: wrap column;
+  border: 1px solid blue;
+  width: 200px;
+`;
+const Input = styled.input`
+  width: 200px;
+`;
+const Button = styled.button`
+  width: 200px;
+`;
+
+const MidContainer = styled.div``;
+const Footer = styled.div``;
 
 class Login extends React.Component {
   state = {
@@ -41,36 +76,40 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.loginSubmit}>
-          <input
-            type='text'
-            name='username'
-            placeholder='Username'
-            onChange={this.usernameChange}
-            value={this.state.usernameVal}
-            required
-          />
-          <input
-            type='text'
-            name='fullname'
-            placeholder='Full name'
-            onChange={this.fullNameChange}
-            value={this.state.fullnameVal}
-            required
-          />
-          <input
-            type='password'
-            name='password'
-            placeholder='Fake password'
-            id='password'
-            value={this.state.pwVal}
-            onChange={this.pwChange}
-            required
-          />
-          <button type='submit'>login</button>
-        </form>
-      </div>
+      <Container>
+        <TopContainer>
+          <Form onSubmit={this.loginSubmit}>
+            <Input
+              type='text'
+              name='username'
+              placeholder='Username'
+              onChange={this.usernameChange}
+              value={this.state.usernameVal}
+              required
+            />
+            <Input
+              type='text'
+              name='fullname'
+              placeholder='Full name'
+              onChange={this.fullNameChange}
+              value={this.state.fullnameVal}
+              required
+            />
+            <Input
+              type='password'
+              name='password'
+              placeholder='Fake password'
+              id='password'
+              value={this.state.pwVal}
+              onChange={this.pwChange}
+              required
+            />
+            <Button type='submit'>login</Button>
+          </Form>
+        </TopContainer>
+        <MidContainer />
+        <Footer />
+      </Container>
     );
   }
 }
